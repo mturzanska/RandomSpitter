8d8706e599a697c4c0be8db270a8103f6f2eccc8
margaret <malgorzata.turzanska@deliveryhero.com>
2016-09-11 04:18:45 +0200

    Add information gain calculation

--- treespitter.py
+++ treespitter.py
@@ -12,15 +12,26 @@ class TreeSpitter(object):
     @staticmethod
     def calculate_entropy(data, class_col):
         entropy = 0
-        set_size = len(data.index)
+        data_size = len(data.index)
         classes = data[class_col].unique()
         for cls in classes:
             members = data.loc[data[class_col] == cls]
             cls_size = len(members)
-            proportion = cls_size/set_size
+            proportion = cls_size/data_size
             entropy -= proportion * log(proportion, 2)
         return entropy
 
-    @staticmethod
-    def calculate_information_gain(data, attr_col):
+    def calculate_information_gain(self, data, class_col, attr):
+        information_gain = self.calculate_entropy(data, class_col)
+        data_size = len(data.index)
+        attr_values = data[attr].unique()
+        for value in attr_values:
+            subset = data.loc[data[attr] == value]
+            subset_size = len(data.index)
+            proportion = subset_size/data_size
+            entropy = self.calculate_entropy(subset, class_col)
+            information_gain -= proportion * entropy
+        return information_gain
+
+    def spit_tree(self):
         pass
