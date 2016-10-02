2282491d0846f33d8be150d5bb4937986e574746
margaret <malgorzata.turzanska@deliveryhero.com>
2016-10-02 17:31:15 +0200

    Move Data creation to fixtures
    Add fixtues for manipulated DataFrames

--- test/test_data.py
+++ test/test_data.py
@@ -1,51 +1,42 @@
 from __future__ import division
-import os
 
 from pandas import DataFrame
 import pytest
 
 from spitter.data import Data
 
-sample_data_file = 'sample_data.csv'
-sample_data_path = os.path.join(os.path.dirname(__file__), sample_data_file)
-class_col = 'good_day'
-
 
 class TestDataInit:
 
-    def test_data_load(self):
-        data = Data(sample_data_path, class_col)
+    def test_data_load(self, data, class_col):
         assert type(data.df) == DataFrame
         assert data.class_col == class_col
         assert data.attr_cols
         assert data.cols == data.attr_cols + [data.class_col]
 
-    def test_class_col_absent(self):
+    def test_class_col_absent(self, data_file):
         absent_class_col = 'not_here'
         with pytest.raises(SystemExit):
             with pytest.raises(ValueError):
-                Data(sample_data_path, absent_class_col)
+                Data(data_file, absent_class_col)
 
 
 class TestCheckForMissing:
 
-    def test_no_missing(self):
-        data = Data(sample_data_path, class_col)
+    def test_no_missing(self, data):
         len_before = len(data.df.index)
         data._check_for_missing()
         len_after = len(data.df.index)
         assert len_before == len_after
 
-    def test_one_missing(self):
-        data = Data(sample_data_path, class_col)
+    def test_one_missing(self, data, class_col):
         data.df[class_col].iloc[0] = None
         len_before = len(data.df.index)
         data._check_for_missing()
         len_after = len(data.df.index)
         assert len_before == len_after + 1
 
-    def test_all_missing(self):
-        data = Data(sample_data_path, class_col)
+    def test_all_missing(self, data, class_col):
         data.df[class_col] = None
         data._check_for_missing()
         assert data.df.empty
@@ -53,36 +44,19 @@ class TestCheckForMissing:
 
 class TestCheckForImbalance:
 
-    def test_no_imbalance(self):
-        data = Data(sample_data_path, class_col)
-        row_count = len(data.df.index)
-        if row_count % 2 == 1:
-            data.df.drop(data.df.tail(1).index, inplace=True)
-            row_count -= 1
-        split = int(row_count / 2)
-        data.df[class_col].iloc[:split] = 1
-        data.df[class_col].iloc[split:] = 0
-        len_before = len(data.df.index)
-        data._check_for_imbalance()
-        len_after = len(data.df.index)
+    def test_no_imbalance(self, balanced_data):
+        len_before = len(balanced_data.df.index)
+        balanced_data._check_for_imbalance()
+        len_after = len(balanced_data.df.index)
         assert len_before == len_after
 
-    def test_imbalance_of_one(self):
-        data = Data(sample_data_path, class_col)
-        row_count = len(data.df.index)
-        if row_count % 2 == 0:
-            data.df.drop(data.df.tail(1).index, inplace=True)
-            row_count -= 1
-        split = int(row_count / 2)
-        data.df[class_col].iloc[:split] = 1
-        data.df[class_col].iloc[split:] = 0
-        len_before = len(data.df.index)
-        data._check_for_imbalance()
-        len_after = len(data.df.index)
+    def test_imbalance_of_one(self, imbalanced_data):
+        len_before = len(imbalanced_data.df.index)
+        imbalanced_data._check_for_imbalance()
+        len_after = len(imbalanced_data.df.index)
         assert len_before == len_after + 1
 
-    def test_max_imbalance(self):
-        data = Data(sample_data_path, class_col)
+    def test_max_imbalance(self, data, class_col):
         data.df[class_col] = 1
         data._check_for_imbalance()
         assert data.df.empty
