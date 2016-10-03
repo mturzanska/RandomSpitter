ca9fa96b1ed1cd3711db116fd81a4c867e5708fe
margaret <malgorzata.turzanska@deliveryhero.com>
2016-10-03 18:14:06 +0200

    Merge Tree and TreeSpitter into one object - WIP

--- random_shrubs/shrubs.py
+++ random_shrubs/shrubs.py
@@ -1,75 +1,84 @@
 from __future__ import division
 from math import log
 
-from tree import Node, Tree
+
+class Node(object):
+
+    def __init__(self, df=[], parent=None, kids=[],
+                 attr=None, attr_value=None):
+        self.df = df
+        self.parent = parent
+        self.kids = kids
+        self.attr = attr
+        self.attr_value = attr_value
 
 
-class TreeSpitter(object):
+class Shrub(object):
 
-    def __init__(self, init_data, init_attrs, class_col):
-        self.init_data = init_data
-        self.init_attrs = init_attrs
+    def __init__(self, df, attr_cols, class_col):
+        self.df = df
+        self.attr_cols = attr_cols
         self.class_col = class_col
-        self.tree = Tree(init_data)
+        self.root = Node(df=self.df)
+        self.nodes = [self.root, ]
+        self.leaves = []
+        while self.stubs:
+            self.grow()
 
-    def compute_entropy(self, data):
+    @staticmethod
+    def compute_entropy(df, class_col):
         entropy = 0
-        data_size = len(data.index)
-        classes = data[self.class_col].unique()
+        data_size = len(df.index)
+        classes = df[class_col].unique()
         for cls in classes:
-            members = data.loc[data[self.class_col] == cls]
+            members = df.loc[df[class_col] == cls]
             cls_size = len(members)
             share = cls_size/data_size
             entropy -= share * log(share, 2)
         return entropy
 
-    def compute_info_gain(self, data, attr):
-        info_gain = self.compute_entropy(data)
-        data_size = len(data.index)
-        if attr in data.columns:
-            attr_values = data[attr].unique()
-            for value in attr_values:
-                subset = data.loc[data[attr] == value]
-                subset_size = len(data.index)
-                share = subset_size/data_size
-                entropy = self.compute_entropy(subset)
-                info_gain -= share * entropy
-            return info_gain
-
-    def choose_attribute(self, data):
-        info_gains = {}
-        for attr in self.init_attrs:
-            info_gain = self.compute_info_gain(data, attr)
-            info_gains[attr] = info_gain
-        return max(info_gains, key=lambda x: info_gains[x])
-
-    def fork(self, data, attr):
+    @staticmethod
+    def fork(df, attr):
         branches = {}
         try:
-            attr_values = data[attr].unique()
+            attr_values = df[attr].unique()
         except KeyError:
             return
         for value in attr_values:
-            branch = data.loc[data[attr] == value]
+            branch = df.loc[df[attr] == value]
             branch = branch.drop(attr, axis=1)
             branches[value] = branch
         return branches
 
-    def unstub_a_stub(self):
-        stub = self.tree.stubs.pop()
-        attr = self.choose_attribute(stub.data)
-        branches = self.fork(stub.data, attr)
-        if not branches:
-            self.tree.leaves.append(stub)
-            return
-        for attr_value, data in branches.iteritems():
-            child = Node(parent=stub, data=data, attr=attr,
-                         attr_value=attr_value)
-            stub.kids.append(child)
-            self.tree.stubs.append(child)
-            self.tree.nodes.append(stub)
+    def compute_info_gain(self, attr):
+        info_gain = self.compute_entropy(df=self.df, class_col=self.class_col)
+        data_size = len(self.df.index)
+        if attr in self.attr_cols:
+            attr_values = self.df[attr].unique()
+            for value in attr_values:
+                subset = self.df.loc[self.df[attr] == value]
+                subset_size = len(self.df.index)
+                share = subset_size/data_size
+                entropy = self.compute_entropy(subset, self.class_col)
+                info_gain -= share * entropy
+            return info_gain
 
-    def spit_tree(self):
-        while self.tree.stubs:
-            self.unstub_a_stub()
-        return self.tree
+    def choose_attribute(self):
+        info_gains = {}
+        for attr in self.attr_cols:
+            info_gain = self.compute_info_gain(attr=attr)
+            info_gains[attr] = info_gain
+        return max(info_gains, key=lambda x: info_gains[x])
+
+    def grow(self):
+        stub = self.stubs.pop()
+        attr = self.choose_attribute()
+        branches = self.fork(stub.df, attr)
+        if not branches:
+            self.shrub.leaves.append(stub)
+            return
+        for attr_value, df in branches.iteritems():
+            child = Node(df=df, parent=stub, attr=attr, attr_value=attr_value)
+            stub.kids.append(child)
+            self.stubs.append(child)
+            self.nodes.append(stub)
